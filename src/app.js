7b146c2ab22e39abf918e031ae1151f8527a4bea
AbdulKhalekG <adnan_akg_1401@hotmail.com>
2022-07-04 20:32:16 -0400

    3

--- src/app.js
+++ src/app.js
@@ -2,7 +2,13 @@ const express = require('express')
 const { route } = require('./routes/index.routes')
 const app = express()
 
-// ROUTES
+
+// middlewares hace referencia a unas funciones que se ejecutan antes de llegar a las rutas
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));  // solo acepta texto
+
+// ROUTES para utilizar las rutas
 
 app.use( require('./routes/index.routes'))
 
--- src/database.js
+++ src/database.js
@@ -13,7 +13,7 @@ const getUsuarios= async () =>{
 
     try{
         
-        const res=await pool.query('select *from usuarios');
+        const res=await pool.query('select * from usuarios');
         console.log(res.rows);
         pool.end();
     } catch (e) {
@@ -22,11 +22,30 @@ const getUsuarios= async () =>{
 };
 
 const insertUsuarios = async () => {
-        const text = 'INSERT INTO usuario (id_usuario, nombre, username, correo, bio, direccion, birthday, clave) VALUES($1, $2, $3, $4, $5, $6, $7, $8)'
+    try {
+        const text = 'INSERT INTO usuarios (id_usuario, nombre, username, correo, bio, direccion, birthday, clave) VALUES($1, $2, $3, $4, $5, $6, $7, $8)'
         const values = ['1', 'Jose ', 'Jose47', 'Jose47@gmail.com', 'Buenas soy Jose y espero', 'amparo', '25', '47jose' ]
 
-        pool.query()
+        
+
+        const res = await pool.query(text, values);
+        console.log(res);
+        pool.end();
+    } catch(e) {
+        console.log(e);
     }
+}
+
+const deleteUser = async () => {
+    const text = 'DELETE FROM usuarios WHERE nombre = $1';
+    const value = ['Jose'];
+
+    const res = await pool.query(text, value);
+    console.log(res)
+
+}
+
+
 
 
 getUsuarios();
